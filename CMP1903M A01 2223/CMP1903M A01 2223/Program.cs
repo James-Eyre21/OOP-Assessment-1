32de0de7886ed02874fe71eed1494a80438c36dc
James Eyre <26464488@students.lincoln.ac.uk>
2023-02-22 19:02:31 +0000

    test 2

--- CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -10,7 +10,10 @@ namespace CMP1903M_A01_2223
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Users name is {name}");
         }
     }
 }
