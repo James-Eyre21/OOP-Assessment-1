73e029702eeb00bd7b96d02d090b4edb08445944
James Eyre <26464488@students.lincoln.ac.uk>
2023-03-04 01:52:42 +0000

    1 shuffle added

--- CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -8,19 +8,19 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
+        public List<Card> cardPack = new List<Card>();
         public Pack()
         {
-            //Initialise the card pack here
-            List<Card> pack = new List<Card>();
+            //A nested for loop to add 13 cards for each suit to the pack
 
             for (int s = 1; s < 5; s++)
             {
                 for (int v = 1; v < 14; v++)
                 {
-                    Card card = new Card();
+                    Card card = new Card(); //Creates a new Card object
                     card.Suit = s;
                     card.Value = v;
-                    pack.Add(card);
+                    cardPack.Add(card);
                 }
             }
         }
@@ -29,12 +29,33 @@ namespace CMP1903M_A01_2223
         {
             //Shuffles the pack based on the type of shuffle
             
+            //Fisher-Yates shuffle
+            if (typeOfShuffle == 1)
+            {
+                Random r = new Random();
+                for (int i = 0; i < cardPack.Count; i++)
+                {
+                    int rndNum = r.Next(0, cardPack.Count); //Creates a random number between 0 and the length of the pack
+                    cardPack.Add(cardPack[rndNum]); //Adds the card to end of the list
+                    cardPack.RemoveAt(rndNum); //Removes the card from its previous position
+                }
+                return true;
+            }
+
+            //Riffle Shuffle
+            else if (typeOfShuffle == 2)
+            {
+                return true;
+            }
+
             return false;
 
         }
         public static Card deal()
         {
             //Deals one card
+            
+
             return null;
 
         }
--- CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +13,14 @@ namespace CMP1903M_A01_2223
         static void Main(string[] args)
         {
 
-            Pack pack1 = new Pack();
-            Console.WriteLine(pack1.pack);
+            Pack pack = new Pack();
+
+            pack.shuffleCardPack(1);
+
+            foreach (Card card in pack.cardPack)
+            {
+                Console.WriteLine($"{card.Suit} {card.Value}");
+            }
 
             Console.ReadKey();
         }
