c0a2266c167cc63f8e83b42626d1c2d42025a3e3
James Eyre <26464488@students.lincoln.ac.uk>
2023-03-05 23:08:22 +0000

    Funcionality done. Needs validation

--- CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,10 @@ namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        public List<Card> cardPack = new List<Card>();
+        public static List<Card> pack = new List<Card>();
         public Pack()
         {
             //A nested for loop to add 13 cards for each suit to the pack
-
             for (int s = 1; s < 5; s++)
             {
                 for (int v = 1; v < 14; v++)
@@ -20,7 +20,7 @@ namespace CMP1903M_A01_2223
                     Card card = new Card(); //Creates a new Card object
                     card.Suit = s;
                     card.Value = v;
-                    cardPack.Add(card);
+                    pack.Add(card);
                 }
             }
         }
@@ -28,41 +28,81 @@ namespace CMP1903M_A01_2223
         public bool shuffleCardPack(int typeOfShuffle)
         {
             //Shuffles the pack based on the type of shuffle
-            
+
             //Fisher-Yates shuffle
             if (typeOfShuffle == 1)
             {
                 Random r = new Random();
-                for (int i = 0; i < cardPack.Count; i++)
+                List<Card> shuffledPack = new List<Card>();
+                for (int i = 0; i < 52; i++)
                 {
-                    int rndNum = r.Next(0, cardPack.Count); //Creates a random number between 0 and the length of the pack
-                    cardPack.Add(cardPack[rndNum]); //Adds the card to end of the list
-                    cardPack.RemoveAt(rndNum); //Removes the card from its previous position
+                    int rndNum = r.Next(0, pack.Count); //Creates a random number between 0 and the length of the pack
+                    shuffledPack.Add(pack[rndNum]); //Adds the card to a new list
+                    pack.RemoveAt(rndNum); //Removes the card from its previous position
                 }
+                pack = shuffledPack; //Sets the value of 'pack' to be the same as the shuffled set of cards. So the shuffled pack cna be accessed elsewhere
                 return true;
             }
 
             //Riffle Shuffle
             else if (typeOfShuffle == 2)
             {
+                List<Card> half1 = new List<Card>(); //Creates a list that will store the first half of the pack
+                List<Card> half2 = new List<Card>(); //Creates a list that will store the second half of the pack
+
+                for (int i = 0; i < pack.Count / 2; i++) //Loops through the first half of the pack
+                {
+                    half1.Add(pack[i]);
+                }
+
+                for (int i = pack.Count / 2; i < pack.Count; i++) //Loops through the second half of the pack
+                {
+                    half2.Add(pack[i]);
+                }
+
+                pack.Clear(); //Clears the values from pack so they can be added back but in a shuffled order
+
+                //This loop should emulate the riffling action, where the cards are split in two and a card from each side is placed on top of the other, one after another
+                for (int i = 0; i < half1.Count; i++)
+                {
+                    pack.Add(half1[i]); //Adds a value from the first half
+                    pack.Add(half2[i]); //Adds a value from the second half
+                }
+
                 return true;
             }
 
-            return false;
+            //No shuffle
+            else if (typeOfShuffle == 3)
+            {
+                return false;
+            }
+
+            else
+            {
+                return false;
+            }
 
         }
-        public static Card deal()
+        public static Card dealCard()
         {
             //Deals one card
-            
+            Card card = pack[0]; //Deals the top card of the pack - simulates how cards are dealt in real life
 
-            return null;
+            return card;
 
         }
         public static List<Card> dealCard(int amount)
         {
             //Deals the number of cards specified by 'amount'
-            return null;
+
+            List<Card> playerCards = new List<Card>();
+            for (int i = 0; i < amount; i++)
+            {
+                playerCards.Add(pack[i]);
+            }
+
+            return playerCards;
         }
     }
 }
---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Testing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Testing
+    {
+        static void Main(string[] args)
+        {
+            Pack pack = new Pack();
+            testPacks(pack, 3); //Test for pack with no shuffle
+            testPacks(pack, 2); //test for pack using Riffle shuffle
+            testPacks(pack, 1); //Test for pack using Fisher-yates shuffle
+            
+
+            Console.ReadKey();
+        }
+
+        //Method to test functionality of program - Shuffling, dealing
+        public static void testPacks(Pack pack, int shuffleType)
+        {
+            pack.shuffleCardPack(shuffleType);
+
+            Card playerCard = Pack.dealCard(); //This will return 1 single card
+            Console.WriteLine($"Your card is: {playerCard.Suit} {playerCard.Value}"); //Prints out the suit then value of the returned card
+
+            Console.WriteLine(); //Prints empty line to distinguish between the single dealt card and the list of cards
+
+            List<Card> playerCards = Pack.dealCard(52); //This will return the given amount of cards and store it in a list
+            //Loops through the list of cards
+            foreach (Card card in playerCards)
+            {
+                Console.WriteLine($"Your card is: {card.Suit} {card.Value}"); //Prints out the suit then value of the current card in the list
+            }
+            Console.WriteLine("----------------"); //Prints a line to make it easier to see when one list of cards has finished printing
+        }
+
+        
+    }
+}
